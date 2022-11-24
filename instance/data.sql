c93260545962597d09d0eb0ac68056f6ea7e1791
jz71 <jz71@evansville.edu>
2022-11-24 14:28:42 +0800

    no message

--- instance/data.sql
+++ instance/data.sql
@@ -3,3 +3,4 @@ INSERT IGNORE  INTO `category` VALUES (2, 'Electronic');
 INSERT IGNORE  INTO `category` VALUES (3, 'Toys');
 INSERT IGNORE  INTO `category` VALUES (4, 'Books');
 INSERT IGNORE  INTO `category` VALUES (5, 'Furnitures');
+INSERT IGNORE  INTO `user` VALUES ('ee9f76d9-040c-401e-b85f-0dea4db8a6c4', 'admin', '18502512547', 1, 'admin@admin.com', NULL, '$2b$12$h6aWHmPY9ojG1qrPUZUr/ucLnEvjo70j2TF7HIajOsHL8Uw5EjHdu');
